21b382c0b905887c9fadf4c1194437b87e401534
Jaswinder Singh <narinderkaur1714@gmail.com>
2025-05-07 18:54:10 +0530

    Login api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,5 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('/tasks', TaskController::class);
+
+Route::post('/login', [AuthController::class, 'login']);
